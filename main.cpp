222a2102fd85ec124c3aef8e030ab2cce2471971
D0mazo <dominykas.simanavicius@proton.me>
2025-04-13 18:09:39 +0300

    Atvirkščiai

--- main.cpp
+++ main.cpp
@@ -6,18 +6,14 @@ using namespace std;
 using namespace std::chrono;
 
 // Constant for array size
-const int SIZE = 100000;
+const int SIZE = 50000;
 
-vector<int> generateRandomArray(int size) {
+// Function to generate a reverse-sorted array of specified size
+vector<int> generateReverseSortedArray(int size) {
     vector<int> arr(size);
-    // Seed random number generator
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(1, 10000);
 
-    // Fill array with random numbers
     for (int i = 0; i < size; i++) {
-        arr[i] = dis(gen);
+        arr[i] = size - i;
     }
     return arr;
 }
@@ -78,7 +74,7 @@ void quickSort(vector<int>& arr, int low, int high) {
 int main() {
     // Generate random array
     // Generate random array
-    vector<int> original = generateRandomArray(SIZE);
+    vector<int> original = generateReverseSortedArray(SIZE);
     // Create copies for sorting
     vector<int> arr1 = original;
     vector<int> arr2 = original;
